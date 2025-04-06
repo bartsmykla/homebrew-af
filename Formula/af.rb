727afd823ef6cbbecd77c88968e2713f7a1b9af0
Bart Smykla <11655498+bartsmykla@users.noreply.github.com>
2025-04-06 09:06:02 +0000

    af: add 0.1.12 bottle.

--- Formula/af.rb
+++ Formula/af.rb
@@ -8,6 +8,13 @@ class Af < Formula
   head "https://github.com/bartsmykla/af.git",
        branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/bartsmykla/af"
+    sha256 cellar: :any,                 arm64_sequoia: "583dc450e8ef08bae070fb056cbb24f0e0e1c8e3f1313108793476923894304d"
+    sha256 cellar: :any,                 ventura:       "cc63995197612751320d3a93fee2ffac0938542aca496003fe9eec9fd1e57a34"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f111dff305b5cd2e058489e45a47ceac5a83b507588b5fd3d3e5ce3e0eac5373"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl"
   depends_on "zlib"
